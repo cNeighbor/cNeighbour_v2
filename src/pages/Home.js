8c7f51b26e3c8ca1a24b6459cf5a7436f45eb8cf
Darlington <Darlingtonnnam@gmail.com>
2022-07-09 01:09:47 +0100

    Register and Login Pages Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,16 @@ import Navbar from "../components/Navbar"
 import Header from "../components/Header"
 import CategoriesLg from '../components/CategoriesLg'
 import Footer from '../components/Footer'
+import Business from '../components/Business'
+import Items from '../components/Items'
 
 const index = () => {
     return (
         <div>
-            <Navbar />
             <Header />
             <CategoriesLg />
+            <Business />
+            <Items />
             <Footer />
         </div>
     )
