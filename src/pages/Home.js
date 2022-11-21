ebd957883c1b714431f460bcf981dc79fd985ad3
Darlington02 <darlingtonnnam@gmail.com>
2022-11-21 03:46:44 +0100

    added carousel to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,17 @@ import Events from '../components/Events'
 import Businesses from '../components/Businesses'
 import Why from '../components/Why'
 import DiscountOffers from '../components/DiscountOffers'
+import Carousel from '../components/Carousel'
 
 const index = () => {
     return (
         <div>
            <Ad text="Reserve online today, browse our wide assortment of rooms at different relaxation locations."/>
+           <Carousel />
            <Categories />
            <DiscountOffers/>
            <Events/>
-           <Businesses />
+           {/* <Businesses /> */}
            <Places/>
            <Why />
         </div>
